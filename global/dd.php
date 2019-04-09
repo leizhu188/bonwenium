0d6ff13987b86147584ce1557be437933f7f605c
weibangwei <122783533@qq.com>
2019-04-09 10:32:43 +0800

    新增 dd

--- global/dd.php
+++ global/dd.php
@@ -1,4 +1,7 @@
 <?php
+use \Symfony\Component\VarDumper\Dumper\CliDumper;
+use \Symfony\Component\VarDumper\Dumper\HtmlDumper;
+use \Symfony\Component\VarDumper\Cloner\VarCloner;
 /**
  * @author bonwe
  * 自定义全局方法
@@ -14,7 +17,7 @@ if (! function_exists('dd')) {
     {
         array_map(function ($x) {
             if (class_exists(CliDumper::class)) {
-                $dumper = 'cli' === PHP_SAPI ? new CliDumper : new HtmlDumper;
+                $dumper = 'cli' === PHP_SAPI ? new CliDumper : new HtmlDumper();
                 $dumper->dump((new VarCloner())->cloneVar($x));
             } else {
                 var_dump($x);
